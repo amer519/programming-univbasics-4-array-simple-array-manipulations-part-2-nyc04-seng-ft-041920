62558bfc47cacd25a5c306ac685fe5b85cbd9dde
Amer Fahmy <afahmy519@gmail.com>
2020-04-28 18:52:38 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,9 @@
+#def using_concat(my_favorite_things, favs)
+ # my_favorite_things.concat (favs)
+#end
+require "pry"  
+  
+def using_insert(array, "python")
+  binding.pry
+  array.insert(4, "python")
+end
